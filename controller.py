3d0f99299ffc3b73f4bcde9bdfbbe2783daca87a
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-08-10 01:09:27 +0300

    controller testing

--- controller.py
+++ controller.py
@@ -41,5 +41,30 @@ while run:
                 print(button_code_to_name[event.button])
             else:
                 print(f"unknown button code: {event.button}")
+    # instantly checking all the buttons (keep in mind when making the xbox_one_controller class)
+    # button_states = [joystick.get_button(i) for i in range(joystick.get_numbuttons())]
+    # print(button_states)
+
+
+    # the D-pad (the cross)
+    # -1,0 => left
+    # 1,0 => right
+    # 0,1 => up
+    # 0,-1 => down
+    hat=joystick.get_hat(0)
+    # print(hat)
+    
+
+    left_x = joystick.get_axis(0)
+    left_y = joystick.get_axis(1)
+    right_x = joystick.get_axis(2)
+    right_y = joystick.get_axis(3)
+    left_trigger = joystick.get_axis(4)  # Values from -1 to 1 (often needs adjustment)
+    right_trigger = joystick.get_axis(5)  # Values from -1 to 1 (often needs adjustment)
+    # print(f"Left joystick: {left_x},{left_y}")
+    # print(f"Right joystick: {right_x},{right_y}")
+    # print(f"Left trigger: {left_trigger}")
+    print(f"Right trigger: {right_trigger}")
+    
 
 pygame.quit()
